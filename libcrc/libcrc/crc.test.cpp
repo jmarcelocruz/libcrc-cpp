2a93f16d01e85ceac5481eec0556da53b4e23cb9
José Marcelo Marques da Cruz <jmarcelocruz99@outlook.com>
2024-08-29 23:21:35 +0100

    refactor: merge similar tests

--- libcrc/libcrc/crc.test.cpp
+++ libcrc/libcrc/crc.test.cpp
@@ -2,34 +2,24 @@
 
 #include <gtest/gtest.h>
 
-TEST(Crc8Suite, ZeroLengthInputReturnsZero) {
-    crc::Crc8 crc{0x07};
-    std::array<uint8_t,0> input{};
+TEST(Crc8Suite, ZeroLengthInputReturnsInitialValue) {
+    crc::Crc8 crc_a{0x07};
+    crc::Crc8 crc_b{0x2F, 0xFF};
+    std::array<uint8_t, 0> input{};
 
-    EXPECT_EQ(crc.crc(&input[0], &input[0]), 0);
+    EXPECT_EQ(crc_a.crc(&input[0], &input[0]), 0);
+    EXPECT_EQ(crc_b.crc(&input[0], &input[0]), 0xFF);
 }
 
 TEST(Crc8Suite, NonZeroLengthInputReturnsExpectedValue) {
-    crc::Crc8 crc{0x07};
+    crc::Crc8 crc_a{0x07};
+    crc::Crc8 crc_b{0x2F, 0xFF};
     std::array<uint8_t,3> input{0x31, 0x32, 0x33};
 
-    EXPECT_EQ(crc.crc(&input[0], &input[1]), 0x97);
-    EXPECT_EQ(crc.crc(&input[0], &input[2]), 0x72);
-    EXPECT_EQ(crc.crc(&input[0], &input[3]), 0xC0);
-}
-
-TEST(Crc8Suite, ZeroLengthInputReturnsInitialValue) {
-    crc::Crc8 crc{0x2F, 0xFF};
-    std::array<uint8_t, 0> input{};
-
-    EXPECT_EQ(crc.crc(&input[0], &input[0]), 0xFF);
-}
-
-TEST(Crc8Suite, NonZeroLengthInputWithInitialValueReturnsExpectedValue) {
-    crc::Crc8 crc{0x2F, 0xFF};
-    std::array<uint8_t,3> input{0x31, 0x32, 0x33};
-
-    EXPECT_EQ(crc.crc(&input[0], &input[1]), 0xB0);
-    EXPECT_EQ(crc.crc(&input[0], &input[2]), 0xBD);
-    EXPECT_EQ(crc.crc(&input[0], &input[3]), 0x56);
+    EXPECT_EQ(crc_a.crc(&input[0], &input[1]), 0x97);
+    EXPECT_EQ(crc_b.crc(&input[0], &input[1]), 0xB0);
+    EXPECT_EQ(crc_a.crc(&input[0], &input[2]), 0x72);
+    EXPECT_EQ(crc_b.crc(&input[0], &input[2]), 0xBD);
+    EXPECT_EQ(crc_a.crc(&input[0], &input[3]), 0xC0);
+    EXPECT_EQ(crc_b.crc(&input[0], &input[3]), 0x56);
 }
